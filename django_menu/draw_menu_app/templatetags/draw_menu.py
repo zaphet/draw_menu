1499f695b7faee902ceb13d1096da8539fb87e71
Denis Zeguzin <to41oter@ya.ru>
2023-02-28 02:01:21 +0300

    refactor, documentation

--- django_menu/draw_menu_app/templatetags/draw_menu.py
+++ django_menu/draw_menu_app/templatetags/draw_menu.py
@@ -8,6 +8,10 @@ register = template.Library()
 
 @register.inclusion_tag('menu_tree.html', takes_context=True)
 def draw_menu(context, menu):
+    """
+    Функция отрисовки древовидного меню.
+    Принимает название таблицы.
+    """
     all_menu_items = Item.objects.filter(menu__name=menu)
     items = []
     primary_menu_items = all_menu_items.filter(parent=None)
@@ -18,13 +22,6 @@ def draw_menu(context, menu):
     else:
         chosen_menu_item = all_menu_items.filter(id=int(chosen_menu_item_id)).first()
         ancestors_ids = get_ancestors_ids(all_menu_items, chosen_menu_item)
-        print('!!!ancestors_ids', ancestors_ids)
-        all_family_childs = all_menu_items.filter(parent__in=ancestors_ids)
-        print('!!!all_family_childs', all_family_childs)
-        # for parent in all_menu_items.filter(id__in=ancestors_ids):
-        #     parent.child_items = [item for item in all_menu_items.filter(parent=parent.id)]
-        #     print('!!!parent.child_items', parent.child_items)
-        #     items.append(parent)
         for item in primary_menu_items:
             if item.id in ancestors_ids:
                 item.child_items = get_childs(all_menu_items, item, ancestors_ids)
@@ -33,32 +30,28 @@ def draw_menu(context, menu):
     menu_query = {
         'menu': menu,
         'items': items,
-        # 'child_items': [item for item in all_family_childs.values()],
     }
 
     return menu_query
 
 
 def get_ancestors_ids(all_menu_items, item):
+    """
+    Возвращает список ID всех предков полученного объекта.
+    """
     ancestors_ids = [item.id, ]
-    # parent = item.parent
-    # print('!!!parent', parent)
-    #
-    # while parent:
-    #     ancestors_ids.append(parent)
-    #     print('!!!ancestors_ids.append(parent)', ancestors_ids)
-    #     # parent = parent.parent
-    #     parent = all_menu_items.filter(id=parent).first().parent
     while item.parent:
         ancestors_ids.append(item.parent.id)
-        print('!!!ancestors_ids.append(item.parent)', ancestors_ids)
         item = all_menu_items.filter(id=item.parent.id).first()
 
     return ancestors_ids
 
 
 def get_childs(all_menu_items, parent, ancestors_ids):
-    # child_items = [item for item in all_menu_items.filter(parent=parent.id)]
+    """
+    Возвращает список всех детей полученного объекта.
+    Если эти дети в списке родителей - рекурсивно вызывает себя для поиска их детей.
+    """
     child_items = []
     for item in all_menu_items.filter(parent=parent.id):
         if item.id in ancestors_ids:
